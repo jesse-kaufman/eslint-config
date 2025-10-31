57aa1e8e7b13eb562e37dfeae40c2af00538ca98
jkaufman <jkaufman@peoples.bank>
2025-10-31 12:12:20 -0500

    chore: disable no-void rule

--- 200_base.js
+++ 200_base.js
@@ -119,7 +119,6 @@ const baseConfig = [
       "no-useless-rename": "error",
       "no-useless-return": "error",
       "no-var": "error",
-      "no-void": "error",
       "require-atomic-updates": "warn",
       "require-await": "error",
       "sort-vars": "error",
