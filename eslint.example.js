7b15fa36d179ae9dd725da01326609ffcb70f856
jkaufman <jkaufman@peoples.bank>
2026-03-26 16:46:22 -0500

    chore: remove aliases and use import-x/resolver in example config

--- eslint.example.js
+++ eslint.example.js
@@ -14,25 +14,10 @@ const workspaces = [
     name: "app/backend-workspace",
     files: ["packages/backend/**"],
     settings: {
-      "import/resolver": {
+      "import-x/resolver": {
         typescript: {
           alwaysTryTypes: true,
           project: "./packages/backend/tsconfig.json",
-        },
-        alias: {
-          map: [
-            ["#config/*", "./packages/backend/src/config"],
-            ["#controllers/*", "./packages/backend/src/controllers/*"],
-            ["#errors/*", "./packages/backend/src/errors/*"],
-            ["#middlewares/*", "./packages/backend/src/middlewares/*"],
-            ["#modules/*", "./packages/backend/src/modules/*"],
-            ["#registries/*", "./packages/backend/src/registries/*"],
-            ["#repositories/*", "./packages/backend/src/repositories/*"],
-            ["#routes/*", "./packages/backend/src/routes/*"],
-            ["#services/*", "./packages/backend/src/services/*"],
-            ["#types", "./packages/backend/src/types"],
-            ["#utils/*", "./packages/backend/src/utils/*"],
-          ],
         },
       },
     },
@@ -43,20 +28,10 @@ const workspaces = [
     name: "app/shared-workspace",
     files: ["packages/shared/**"],
     settings: {
-      "import/resolver": {
+      "import-x/resolver": {
         typescript: {
           alwaysTryTypes: true,
           project: "./packages/shared/tsconfig.json",
-        },
-        alias: {
-          map: [
-            ["#config/*", "./packages/shared/src/config/*"],
-            ["#errors/*", "./packages/shared/src/errors/*"],
-            ["#fields/*", "./packages/shared/src/fields/*"],
-            ["#fields", "./packages/shared/src/fields/index.js"],
-            ["#utils/*", "./packages/shared/src/utils/*"],
-            ["#validation*/", "./packages/shared/src/validation/*"],
-          ],
         },
       },
     },
@@ -81,26 +56,10 @@ const workspaces = [
     name: "app/frontend-workspace",
     files: ["packages/frontend/**/*"],
     settings: {
-      "import/resolver": {
+      "import-x/resolver": {
         typescript: {
           alwaysTryTypes: true,
           project: "./packages/frontend/tsconfig.json",
-        },
-        alias: {
-          map: [
-            ["#App/*", "./packages/frontend/src/components/App/*"],
-            ["#Base/*", "./packages/frontend/src/components/Base/*"],
-            ["#composables/*", "./packages/frontend/src/composables/*"],
-            ["#config/*", "./packages/frontend/src/config/*"],
-            ["#layouts/*", "./packages/frontend/src/layouts/*"],
-            ["#Projects/*", "./packages/frontend/src/components/Projects/*"],
-            ["#services/*", "./packages/frontend/src/services/*"],
-            ["#stores/*", "./packages/frontend/src/stores/*"],
-            ["#types", "./packages/frontend/src/types"],
-            ["#UI/*", "./packages/frontend/src/components/UI/*"],
-            ["#utils/*", "./packages/frontend/src/utils/*"],
-            ["#views/*", "./packages/frontend/src/views/*"],
-          ],
         },
       },
     },
